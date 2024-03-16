186e48be4aeb29eb8967671fe8e58520f9dbcf97
Krishanu Saha <93394632+krishanu7@users.noreply.github.com>
2024-03-16 15:52:35 +0530

    Basic Fronted Setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
+const navLinks = [
+  {
+    path: '/home',
+    display: "Home",
+  },
+  {
+    path: '/doctors',
+    display: "Find a Doctor",
+  },
+  {
+    path: '/service',
+    display: 'Services'
+  },
+  {
+    path: '/contact',
+    display: 'Contact'
+  },
+]
+
 const Home = () => {
   return (
     <div>Home</div>
