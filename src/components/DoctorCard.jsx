5c5f36a90819aa1409d2356801ff646c94c9e290
Krishanu Saha <93394632+krishanu7@users.noreply.github.com>
2024-03-17 02:37:10 +0530

    DoctorCard

--- src/components/DoctorCard.jsx
+++ src/components/DoctorCard.jsx
@@ -4,27 +4,26 @@ import { BiRightArrowAlt } from "react-icons/bi";
 import { FaStar } from "react-icons/fa6";
 
 const DoctorCard = ({ doctor }) => {
-  const { name, specialization, experiences } = doctor;
-  const {} = doctor;
+  const { name, specialization, experience } = doctor;
   return (
-     <div className='p-3 lg:p-5'>
+    <div className='p-3 lg:p-5 shadow-panelShadow rounded-sm'>
       <div>
         <img className='w-full' src="/" alt="" />
       </div>
       <h2 className='text-lg lg:text-xl text-headingColor font-[700] mt-3 lg:mt-5'>{name}</h2>
       <div className='mt-2 lg:mt-4 flex items-center justify-between'>
-        <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-2 lg:py-2 lg:px-6 text-[12px] lg:text-[16px] font-semibold rounded'>
+        <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-2 lg:py-2 lg:px-6 text-md lg:text-lg font-semibold rounded'>
           {specialization}
         </span>
         <div className='flex items-center gap-2'>
           <span className='flex items-center gap-1 text-md lg:text-lg font-semibold text-headingColor'>
-            <FaStar className='text-lg md:text-xl text-yellow-400'/> (5)
+            <FaStar className='text-lg md:text-xl text-yellow-400' /> (5)
           </span>
         </div>
       </div>
       <div className='mt-[14px] lg:mt-3 flex items-center justify-between'>
         <p className='text-[14px] font-[400] text-textColor'>
-          At {experiences && experiences[0]?.hospital}
+          Experience of {" "} {experience} {" "} years
         </p>
         <div>
           <Link to={`/doctors/${doctor._id}`}
