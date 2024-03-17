e517237c0bc6879389436fa53d7e98e52008f5c9
Sumit Agarwal <agawralsumit334ju@gmail.com>
2024-03-17 14:14:16 +0530

    Backend Deployed URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "http://localhost:8000/api";
+// export const BASE_URL = "http://localhost:8000/api";"
+export const BASE_URL = "https://krishna-lageche-backend.onrender.com/api"
 export const token = localStorage.getItem('token');
